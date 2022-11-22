e1a63ba2ddc3d4cd13e08bc573bb5c05fa0af44d
Andr3s593 <rac.piedra@yavirac.edu.ec>
2022-11-21 21:46:15 -0500

    Deber 2 Not found and register

--- proyecto1/src/app/app.module.ts
+++ proyecto1/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
@@ -13,6 +12,8 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { LoginComponent } from './auth/login/login.component';
 import { BreadcrumbsComponent } from './static/breadcrumbs/breadcrumbs.component';
+import { AppRoutingModule } from './app-routing.module';
+import { PagesComponent } from './pages/pages.component';
 
 
 @NgModule({
@@ -28,11 +29,12 @@ import { BreadcrumbsComponent } from './static/breadcrumbs/breadcrumbs.component
     DashboardComponent,
     NotFoundComponent,
     LoginComponent,
-    BreadcrumbsComponent
-    
+    BreadcrumbsComponent,
+    PagesComponent        
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
